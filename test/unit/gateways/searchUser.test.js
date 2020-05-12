ebf5ad29d8147b1a92a467e0f964e27f47cc2fa8
Mariastakh <astakhovam94@gmail.com>
2020-05-12 09:07:24 +0100

    fix error throwing capability in the login usecase

--- test/unit/gateways/searchUser.test.js
+++ test/unit/gateways/searchUser.test.js
@@ -2,29 +2,53 @@ const gateway = require("../../../lib/gateways/searchUser");
 
 const createGateway = (user, error) => {
   db = {
-    query: jest.fn(async () => {
+    query: jest.fn(() => {
       if (error) {
+        //console.log("inside db error ");
         throw new Error("Database error");
-      }
-      return user;
+      } else 
+        return user;
+      
     }),
   };
 
   return gateway({ db });
 };
 
-describe("gateway", () => {
-  it("queries the database with the correct query", async () => {
-    const gateway = createGateway([]);
-    const user = { username: "Maria" };
+describe("searchUserTest", () => {
+  let user, gateway, result;
 
-    await gateway.execute(user);
+  describe("when we call the gateway with a valid user", () => {
+    beforeEach(() => {
+      user = { username: "Maria" };
+      gateway = createGateway([user]);
+      result = gateway.execute(user);
+    });
 
-    expect(
-      db.query
-    ).toHaveBeenCalledWith(
-      "SELECT username, password FROM member where username =$1",
-      ["Maria"]
-    );
+    it("should call the database with a valid query", () => {
+      expect(
+        db.query
+      ).toHaveBeenCalledWith(
+        "SELECT username, password FROM member where username =$1",
+        ["Maria"]
+      );
+    });
+
+    it("should return a user", () => {
+      expect(result.length).toBe(1);
+    });
+  });
+
+  describe("when we call the gateway with an non-existant user", () => {
+    beforeEach(() => {
+      user = { username: "nonexistant" };
+      const error = true;
+      gateway = createGateway(user, true);
+      result = gateway.execute(user);
+    });
+
+    it("should throw an error", () => {
+      expect(gateway.execute(user)).toThrowError(new Error('yuck, octopus flavor'));
+    });
   });
 });
