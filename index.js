da078360033e9e650cdfec3a7585c4ea2e9f482e
Mariastakh <astakhovam94@gmail.com>
2020-05-14 12:09:27 +0100

    receive coordinates from the client

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(expressSanitizer());
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  
+
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
@@ -69,7 +69,7 @@ app.post("/", async (req, res, next) => {
       username: sanitizedUsername,
       password: sanitizedPassword,
     };
-    
+
     await login({
       user: user,
       gateway: searchUser({ user: user, db: dbConnection }),
@@ -84,14 +84,16 @@ app.post("/", async (req, res, next) => {
 
 app.get("/dashboard", async (req, res, next) => {
   try {
-    const weather = await weather();
-    const news = await getNews();
-    const football = await getFootballUpdate();
-    const todoList = await getToDoList();
-    const warmer = await getWarmer();
-    const photos = await getPhotos();
-    const dashboard = [weather, news, football, toDoList, warmer, photos];
-    res.send(dashboard);
+    const weatherReport = await weather();
+    // const news = await getNews();
+    // const football = await getFootballUpdate();
+    // const todoList = await getToDoList();
+    // const warmer = await getWarmer();
+    // const photos = await getPhotos();
+    const dashboard = [weatherReport];
+    res.json({
+      weather: weatherReport,
+    });
   } catch (err) {
     next(err);
   }
@@ -131,10 +133,14 @@ app.get("/photos", (req, res, next) => {
   }
 });
 
-app.get("/weather", async (req, res, next) => {
+app.post("/weather", async (req, res, next) => {
   try {
-    const weatherUpdate = await weather();
-    res.send(weatherUpdate);
+    const location = req.body;
+    const weatherReport = await weather(location);
+
+    res.json({
+      weather: weatherReport,
+    });
   } catch (err) {
     next(err);
   }
