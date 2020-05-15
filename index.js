0416d78470bac26d95b2e5d4428eb5bfdfffa810
Mariastakh <astakhovam94@gmail.com>
2020-05-15 15:06:56 +0100

    remove console logs

--- index.js
+++ index.js
@@ -140,12 +140,11 @@ app.post("/", async (req, res, next) => {
 });
 
 app.post("/upload-image", async (req, res, next) => {
-  console.log("in request");
-
-    const s3 = new aws.S3(); // Create a new instance of S3
+    const s3 = new aws.S3(); 
     const fileName = req.body.fileName;
     const fileType = req.body.fileType;
-    // Set up the payload of what we are sending to the S3 api
+
+    // Set up the payload to send to the s3 api
     const s3Params = {
       Bucket: S3_BUCKET,
       Key: fileName,
@@ -153,6 +152,7 @@ app.post("/upload-image", async (req, res, next) => {
       ContentType: fileType,
       ACL: "public-read",
     };
+    
     // Make a request to the S3 API to get a signed URL which we can use to upload our file
     s3.getSignedUrl("putObject", s3Params, (err, data) => {
       
@@ -160,13 +160,14 @@ app.post("/upload-image", async (req, res, next) => {
         console.log(err);
         res.json({ success: false, error: err });
       }
-      // Data payload of what we are sending back, the url of the signedRequest and a URL where we can access the content after its saved.
+
+      // Data payload of what we are sending back, 
+      // the url of the signedRequest and a URL where we can access the content after its saved.
       const returnData = {
         signedRequest: data,
         url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
       };
-      // Send it all back
-      console.log(returnData)
+
       res.json({ success: true, data: { returnData } });
     });
   
